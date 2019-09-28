7ecfc27292ed2ed22142284df70f88101a8d01a9
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2019-09-28 13:51:29 +0600

    dashboard view implement and url configure

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from monitoring.views import DashboardView
+
+urlpatterns = [
+    path('', DashboardView.as_view(), name='dashboard')
 ]
--- monitoring/views.py
+++ monitoring/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+__author__ = 'Shafikur Rahman'
+
+
+class DashboardView(TemplateView):
+    template_name = 'monitoring/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['title'] = 'Dashboard'
+        context['page_headline'] = 'Dashboard'
+        return context
